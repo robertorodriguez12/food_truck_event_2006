a87d8f0e91f5e1d009d62dc2f1f17581d8146627
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-08-05 10:08:35 -0600

    Add test for sorted item list

--- test/event_test.rb
+++ test/event_test.rb
@@ -128,4 +128,26 @@ class EventTest < MiniTest::Test
 
     assert_equal [item1], event.overstocked_items
   end
+
+  def test_it_can_list_items_sorted_by_alphabetical_order
+    event = Event.new("South Pearl Street Farmers Market")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    expected = ["Apple Pie (Slice)", "Banana Nice Cream", "Peach Pie (Slice)", "Peach-Raspberry Nice Cream"]
+    assert_equal expected, event.sorted_item_list
+  end
 end
