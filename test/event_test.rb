8a92d90b187da56de66b65aaa485c7a13c00d65f
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-08-05 09:21:02 -0600

    Create test for add food truck method

--- test/event_test.rb
+++ test/event_test.rb
@@ -15,4 +15,25 @@ class EventTest < MiniTest::Test
   end
 
 
+  def test_it_can_add_food_trucks_and_items
+    event = Event.new("South Pearl Street Farmers Market")
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3.stock(item1, 65)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+
+    expected = [food_truck1, food_truck2, food_truck3]
+    assert_equal expected, event.food_trucks
+  end
 end
